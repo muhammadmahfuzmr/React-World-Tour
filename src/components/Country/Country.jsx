6222b1190ef41e8696f0f9aad91294778c34c6e1
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-10-25 07:30:26 +0600

    added area and population in ui

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,15 @@
-
-import '../Country/Country.css'
-const Country = ({country}) => {
-    const {name , flags}=country
-    return (
-        <div className="country-box">
-            <h4>Name: {name.official}</h4>
-            <img src={flags.png} alt="" />
-        </div>
-    );
+import "../Country/Country.css";
+const Country = ({ country }) => {
+  console.log(country);
+  const { name, flags, area, population } = country;
+  return (
+    <div className="country-box">
+      <h4>Name: {name.official}</h4>
+      <img src={flags.png} alt="" />
+      <p>Population: {population} </p>
+      <p>Area: {area}</p>
+    </div>
+  );
 };
 
 export default Country;
